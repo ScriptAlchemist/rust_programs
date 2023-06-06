ab67efdacb42bbdbdb6cb590718d6823b713ab54
Cardano G.O.A.T <Jrkbender@gmail.com>
2023-06-05 23:47:54 -0400

    finished open last rust program to open last blog post

---
+++ open_last/src/main.rs
@@ -0,0 +1,49 @@
+use std::fs;
+#[cfg(unix)]
+use std::os::unix::fs::MetadataExt;
+use std::process::Command;
+use std::time::SystemTime;
+
+#[cfg(windows)]
+use std::os::windows::prelude::*;
+
+fn main() -> std::io::Result<()> {
+
+    let dir = ".";
+
+    let entry = fs::read_dir(dir)?
+        .filter_map(|res| res.ok())
+        .filter_map(|entry| fs::metadata(&entry.path()).map(|meta| (entry.path(), meta)).ok())
+        .max_by_key(|(_, meta)| get_last_modified_time(meta.clone()).unwrap_or(SystemTime::UNIX_EPOCH))
+        .map(|(path, _)| path);
+
+    if let Some(newest_file_path) = entry {
+
+        #[cfg(windows)]
+        let mut child = Command::new("vim.bat")
+            .arg(newest_file_path)
+            .spawn()
+            .expect("Failed to open file in Vim");
+
+        #[cfg(unix)]
+        let mut child = Command::new("vim.exe")
+            .arg(newest_file_path)
+            .spawn()
+            .expect("Failed to open file in Vim");
+
+        let _ = child.wait();
+    } else {
+        println!("No files found in directory {}", dir);
+    }
+    Ok(())
+}
+
+#[cfg(unix)]
+fn get_last_modified_time(meta: fs::Metadata) -> std::io::Result<SystemTime> {
+    meta.modified()
+}
+
+#[cfg(windows)]
+fn get_last_modified_time(meta: fs::Metadata) -> std::io::Result<SystemTime> {
+    Ok(SystemTime::UNIX_EPOCH + std::time::Duration::from_secs((meta.last_write_time() / 10_000_000) as u64))
+}
