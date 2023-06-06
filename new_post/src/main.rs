4cd7828336dded363c2a478078bfd82351a9c061
Cardano G.O.A.T <Jrkbender@gmail.com>
2023-06-05 23:24:27 -0400

    finished blog isosec generation and vim opening to start blog post

---
+++ new_post/src/main.rs
@@ -0,0 +1,45 @@
+use std::fs::File;
+use std::io::Write;
+use std::process::Command;
+use chrono::{Utc, Datelike, Timelike};
+
+fn main() -> Result<(), std::io::Error> {
+    let now = Utc::now();
+    let filename = format!("{}{}{}{}{}{}.md",
+        now.year(),
+        format!("{:02}", now.month()),
+        format!("{:02}", now.day()),
+        format!("{:02}", now.hour()),
+        format!("{:02}", now.minute()),
+        format!("{:02}", now.second()));
+
+    let mut file = File::create(filename.clone())?;
+    let formatted_now = now.to_rfc3339_opts(chrono::SecondsFormat::Millis, true);
+
+    let content = format!(r#"---
+title: ''
+excerpt: ''
+coverImage: '/assets/blog/'
+date: '{}'
+author:
+  name: Justin Bender
+  picture: '/assets/blog/authors/Bender.png'
+ogImage:
+  url: '/assets/blog/'
+---
+
+## Title
+
+    "#, formatted_now);
+
+    file.write_all(content.as_bytes())?;
+
+    // Open Vim
+    let mut child = Command::new("vim.bat")
+        .arg(&filename)
+        .spawn()
+        .expect("Failed to open file in Vim");
+
+    let _ = child.wait();
+    Ok(())
+}
